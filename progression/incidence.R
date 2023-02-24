ec0ef44246118f38a89e41aaa82561ab27379bee
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2023-02-24 13:48:19 +0000

    updating inc based on 1 year infection

--- progression/incidence.R
+++ progression/incidence.R
@@ -33,7 +33,7 @@ load(file=gh('LTBI/data/rnra.Rdata'))
 rnra <- merge(rnra,pzz,by='acat',all.x=TRUE)
 rnra[,prog.recent:=rbeta(nrow(rnra),shape1=A,shape2=B)]
 rnra[,prog.slow:=rlnorm(nrow(rnra),meanlog=eps$meanlog,sdlog=eps$sdlog)]
-rnra[,inc0:=(P2) * prog.recent + (P-P2) * prog.slow] #baseline incidence
+rnra[,inc0:=(P1) * prog.recent + (P-P1) * prog.slow] #baseline incidence
 
 ## notification data for comparison
 fn <- gh('progression/data/NR.Rdata')
@@ -52,8 +52,8 @@ if(file.exists(fn)){
 }
 
 ## aggregate over ages
-rnr <- rnra[,.(P=mean(P),P2=mean(P2),inc0=mean(inc0)),
-               by=.(iso3,acat,replicate)] #mean over ages
+rnr <- rnra[,.(P=mean(P),P1=mean(P1),inc0=mean(inc0)),
+               by=.(iso3,acat,replicate,mixing)] #mean over ages
 
 ## relative risk data
 load(gh('PAF/data/IRR.Rdata'))
@@ -64,7 +64,7 @@ rnr <- merge(rnr,
                     h,irr,thin,IRRthin)],
              by=c('iso3','acat','replicate'))
 rnr[,LTBI:=P*value]
-rnr[,LTBI2:=P2*value]
+rnr[,LTBI1:=P1*value]
 rnr[,inc.num0:=inc0*value]
 rnr[,inc:=inc0 * (1-h+h*irr)*(1-thin + thin*IRRthin)]
 rnr[,inc.num:=inc*value]
@@ -73,17 +73,18 @@ rnr[,inc.num:=inc*value]
 ## TODO check uncertainty
 ## TOTAL
 rnrtot <- rnr[,.(P=weighted.mean(P,value),
-                 P2=weighted.mean(P2,value),
+                 P1=weighted.mean(P1,value),
                  inc0=weighted.mean(inc0,value),
                  inc=weighted.mean(inc,value),
                  LTBI=sum(LTBI),
-                 LTBI2=sum(LTBI2),
+                 LTBI1=sum(LTBI1),
                  inc.num0=sum(inc.num0),
                  inc.num=sum(inc.num),
                  value=sum(value)
-                 ), by=.(acat,replicate)]
+                 ), by=.(acat,replicate,mixing)]
 rnrtot[,iso3:='TOTAL']
 names(rnr)
+
 (tnmz <- names(rnrtot))
 rnr <- rbind(rnr[,..tnmz],rnrtot) #TODO
 lvls <- rnr[,unique(iso3)]
@@ -97,9 +98,9 @@ rnr$iso3 <- factor(rnr$iso3,levels=lvls,ordered = TRUE)
 rnrss <- rnr[,.(P.mid=mean(P),inc0.mid=mean(inc0),inc.mid=mean(inc),
                 LTBI.mid=mean(LTBI),
                 inc.num0.mid=mean(inc.num0),inc.num.mid=mean(inc.num),
-                LTBI2.mid=mean(LTBI2),P2.mid=mean(P2),
-                LTBI2.lo=lo(LTBI2),P2.lo=mean(P2),
-                LTBI2.hi=hi(LTBI2),P2.hi=mean(P2),
+                LTBI1.mid=mean(LTBI1),P1.mid=mean(P1),
+                LTBI1.lo=lo(LTBI1),P1.lo=mean(P1),
+                LTBI1.hi=hi(LTBI1),P1.hi=mean(P1),
                 P.lo=lo(P),P.hi=hi(P),
                 inc0.hi=hi(inc0),inc0.lo=lo(inc0),
                 inc.hi=hi(inc),inc.lo=lo(inc),
@@ -107,13 +108,13 @@ rnrss <- rnr[,.(P.mid=mean(P),inc0.mid=mean(inc0),inc.mid=mean(inc),
                 inc.num0.hi=hi(inc.num0),inc.num0.lo=lo(inc.num0),
                 inc.num.hi=hi(inc.num),inc.num.lo=lo(inc.num)
                 ),
-             by=.(iso3,acat)] #mean/hi/lo
+             by=.(iso3,acat,mixing)] #mean/hi/lo
 
-smy <- rnrss[,.(iso3,acat,
+smy <- rnrss[,.(iso3,acat,mixing,
                 LTBI.mid,LTBI.lo,LTBI.hi,
                 inc.num0.mid,inc.num0.lo,inc.num0.hi,
                 inc.num.mid,inc.num.lo,inc.num.hi,
-                LTBI2.mid,LTBI2.lo,LTBI2.hi,
+                LTBI1.mid,LTBI1.lo,LTBI1.hi,
                 inc.mid,inc.lo,inc.hi,
                 inc0.mid,inc0.lo,inc0.hi
                 )]
@@ -121,12 +122,12 @@ smy[,LTBI.fmt:=fmt(LTBI.mid,LTBI.lo,LTBI.hi)]
 smy[,inc.num0.fmt:=fmt(inc.num0.mid,inc.num0.lo,inc.num0.hi)]
 smy[,inc.num.fmt:=fmt(inc.num.mid,inc.num.lo,inc.num.hi)]
 smy[,.(iso3,acat,LTBI.fmt,inc.num0.fmt,inc.num.fmt)]
-smys <- smy[,.(iso3,acat,LTBI.fmt,
+smys <- smy[,.(iso3,mixing,acat,LTBI.fmt,
                inc.num0.fmt,inc.num.fmt,
               LTBI.mid,LTBI.lo,LTBI.hi,
               inc.num0.mid,inc.num0.lo,inc.num0.hi,
               inc.num.mid,inc.num.lo,inc.num.hi)]
-print(smy[iso3=='TOTAL',.(sum(inc.num0.mid)/1e6,sum(inc.num.mid)/1e6)])
+print(smy[iso3=='TOTAL' & mixing=='assortative',.(sum(inc.num0.mid)/1e6,sum(inc.num.mid)/1e6)])
 
 fwrite(smy,file=gh('outdata/smy.csv')) #~1.4M or 1.7M
 save(smy,file=gh('progression/data/smy.Rdata'))
@@ -139,22 +140,23 @@ tmp <- smy[!is.na(CDR),.(iso3,acat,CDR0,CDR)]
 fwrite(tmp,file=gh('outdata/CDR.csv'))
 
 ## reformat for plotting
-smy2 <- smy[,.(iso3,acat,notified,
+smy2 <- smy[,.(iso3,mixing,acat,notified,
                inc.num0.mid,inc.num0.lo,inc.num0.hi,
                inc.num.mid,inc.num.lo,inc.num.hi
                )]
-smy2 <- melt(smy2,id=c('iso3','acat','notified'))
+smy2 <- melt(smy2,id=c('iso3','acat','notified','mixing'))
 smy2[,method:='with risk factors']
 smy2[grepl('0',variable),method:='without risk factors']
 smy2[,variable:=gsub('0','',variable)]
-smy2 <- dcast(smy2,iso3+acat+notified+method~variable,value.var = 'value')
+smy2 <- dcast(smy2,iso3+acat+notified+method + mixing ~ variable,value.var = 'value')
 
 ## plot
 m <- 1e6*0.75
 plt <- ggplot(smy2,aes(x=notified,y=inc.num.mid,
                       ymin=inc.num.lo,ymax=inc.num.hi,
-                      label=iso3,col=method)) +
-  geom_point(shape=1,size=2) +
+                      label=iso3,shape=mixing,
+                      col=method)) +
+  geom_point(size=2) +
   geom_errorbar(width=10,alpha=0.75)+
   geom_text_repel(show.legend = FALSE)+
   scale_x_sqrt(limits=c(0,m),label=comma)+
@@ -174,55 +176,55 @@ ggsave(plt,file=here('plots/IvN.png'),h=7,w=14)
 ## quick compare with KS data
 load('~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC.Rdata')
 
-TBC
-smy2
+## TBC
+## smy2
 
-smy2 <- merge(smy2,TBC[,.(iso3,KSI=incidence,acat)],
-              by=c('iso3','acat'),all.x=TRUE,all.y=FALSE)
+## smy2 <- merge(smy2,TBC[,.(iso3,KSI=incidence,acat)],
+##               by=c('iso3','acat'),all.x=TRUE,all.y=FALSE)
 
-plt <- ggplot(smy2,aes(x=KSI,y=inc.num.mid,
-                ymin=inc.num.lo,ymax=inc.num.hi,
-                label=iso3,col=method)) +
-  geom_point(shape=1,size=2) +
-  geom_errorbar(width=10,alpha=0.75)+
-  geom_text_repel(show.legend = FALSE)+
-  scale_x_sqrt(limits=c(0,m),label=comma)+
-  scale_y_sqrt(limits=c(0,m),label=comma)+
-  geom_abline(slope=1,intercept = 0,col=2)+
-  facet_wrap(~acat)+coord_fixed()+# + xlim(0,m)+ylim(0,m)+
-  ylab('Estimated incidence 2019 (sqrt scale)')+
-  xlab('KS estimate TB 2017 (sqrt scale)')+
-  theme_light()+theme(legend.position = 'top')
-plt
+## plt <- ggplot(smy2,aes(x=KSI,y=inc.num.mid,
+##                 ymin=inc.num.lo,ymax=inc.num.hi,
+##                 label=iso3,col=method)) +
+##   geom_point(shape=1,size=2) +
+##   geom_errorbar(width=10,alpha=0.75)+
+##   geom_text_repel(show.legend = FALSE)+
+##   scale_x_sqrt(limits=c(0,m),label=comma)+
+##   scale_y_sqrt(limits=c(0,m),label=comma)+
+##   geom_abline(slope=1,intercept = 0,col=2)+
+##   facet_wrap(~acat)+coord_fixed()+# + xlim(0,m)+ylim(0,m)+
+##   ylab('Estimated incidence 2019 (sqrt scale)')+
+##   xlab('KS estimate TB 2017 (sqrt scale)')+
+##   theme_light()+theme(legend.position = 'top')
+## plt
 
-ggsave(plt,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC.png')
+## ggsave(plt,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC2.png')
 
-tmp <- smy2[iso3!='TOTAL',.(Snow=sum(KSI)/1e6,prog=sum(inc.num.mid)/1e6),by=.(acat,method)]
-tmp
-fwrite(tmp,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/tbc.csv')
+## tmp <- smy2[iso3!='TOTAL',.(Snow=sum(KSI)/1e6,prog=sum(inc.num.mid)/1e6),by=.(acat,method,mixing)]
+## tmp
+## fwrite(tmp,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/tbc.csv')
 
-## comparison of totals
-tot <- smy2[iso3!='TOTAL',.(snowtot=sum(KSI),inc=sum(inc.num.mid)),by=.(acat,method)]
-tot[,inc/snowtot]
-tot <- smy2[iso3!='TOTAL',.(snowtot=sum(KSI),inc=sum(inc.num.mid)),by=.(method)]
-tot[,inc/snowtot]
+## ## comparison of totals
+## tot <- smy2[iso3!='TOTAL',.(snowtot=sum(KSI),inc=sum(inc.num.mid)),by=.(acat,method,mixing)]
+## tot[,inc/snowtot]
+## tot <- smy2[iso3!='TOTAL',.(snowtot=sum(KSI),inc=sum(inc.num.mid)),by=.(method,mixing)]
+## tot[,inc/snowtot]
 
 
-m <- 1e6*0.75
-plt <- ggplot(smy2,aes(x=notified,y=KSI,
-                       label=iso3,col=method)) +
-  geom_point(shape=1,size=2) +
-  geom_text_repel(show.legend = FALSE)+
-  scale_x_sqrt(limits=c(0,m),label=comma)+
-  scale_y_sqrt(limits=c(0,m),label=comma)+
-  geom_abline(slope=1,intercept = 0,col=2)+
-  facet_wrap(~acat)+coord_fixed()+# + xlim(0,m)+ylim(0,m)+
-  ylab('KSI 2017 (sqrt scale)')+
-  xlab('Notified TB 2019 (sqrt scale)')+
-  theme_light()+theme(legend.position = 'top')
-plt
+## m <- 1e6*0.75
+## plt <- ggplot(smy2,aes(x=notified,y=KSI,
+##                        label=iso3,col=method)) +
+##   geom_point(shape=1,size=2) +
+##   geom_text_repel(show.legend = FALSE)+
+##   scale_x_sqrt(limits=c(0,m),label=comma)+
+##   scale_y_sqrt(limits=c(0,m),label=comma)+
+##   geom_abline(slope=1,intercept = 0,col=2)+
+##   facet_wrap(~acat)+coord_fixed()+# + xlim(0,m)+ylim(0,m)+
+##   ylab('KSI 2017 (sqrt scale)')+
+##   xlab('Notified TB 2019 (sqrt scale)')+
+##   theme_light()+theme(legend.position = 'top')
+## plt
 
-ggsave(plt,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC2.png')
+## ggsave(plt,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC2.png')
 
 
 ## TODO list
@@ -230,3 +232,4 @@ ggsave(plt,file='~/Dropbox/Documents/comms/KatharinaKranzer/ado2/graphs/TBC2.png
 ## compare snow, compare IHME?
 ## include w/ and w/o mixing
 ## go back over checks in LTBI code
+## check snow comparison
